60ac4ce2dbbfbb145b46ed0983d99c1d53b79e3a
Junior Silva <juniorjds2015@gmail.com>
2022-03-06 16:31:49 -0300

    fix: 🐛 removed observations outside of nyc

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -36,8 +36,8 @@ def go(args):
     dataframe['last_review'] = pd.to_datetime(dataframe['last_review'])
 
     # Remove observations outside of NYC
-    # idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
-    # df = df[idx].copy()
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     logger.info(f'Saving Dataframe {args.output_artifact}')
     dataframe.to_csv('clean_sample.csv', index=False)
